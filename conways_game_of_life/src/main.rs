a77fed09c399862aaa199cbf6759e63da8130317
Abena Laast <slaast2@illinois.edu>
2022-04-12 21:14:45 -0500

    added functions:  evolve, count_live_neighbors, evolve_live_cell, evolve_dead_cell. changed starting board and called evolve a few times in main function.

--- conways_game_of_life/src/main.rs
+++ conways_game_of_life/src/main.rs
@@ -1,4 +1,4 @@
-
+// i think this is for display/UI right? i did not need it for backend
 #[derive(Debug, Default, Clone, PartialEq)]
 pub struct Cell {
     pub alive: bool,
@@ -24,6 +24,18 @@ impl GameBoard {
             width,
             board: vec![vec![false; width]; height]
         }
+    }
+
+    pub fn get_board(&self) -> &Vec<Vec<bool>> {
+        &self.board
+    }
+
+    pub fn get_height(&self) -> usize {
+        self.height
+    }
+
+    pub fn get_width(&self) -> usize {
+        self.width
     }
 
     // update the game board with the data in a new game board, 
@@ -58,6 +70,7 @@ impl GameBoard {
         }
     }
 
+    // sets the board element at (row, col) to the alive parameter (true/false)
     pub fn set_cell(&mut self, alive: bool, row: usize, col: usize) {
         if row >= self.height || col >= self.width {
             panic!("invalid board position!");
@@ -65,24 +78,100 @@ impl GameBoard {
         self.board[row][col] = alive;
     }
 
-    pub fn get_board(&self) -> &Vec<Vec<bool>> {
-        &self.board
+    // evolves the elements of the board
+    pub fn evolve(&mut self) {
+        // copy of self's board to evolve
+        let mut future = GameBoard::new(self.height,self.width);
+        future.update(self);
+        // iterate through future copy (starts out same as self's)
+        for i in 0..future.height {
+            for j in 0..future.width {
+                if self.board[i][j] == true { // if cell at location is live
+                    future.evolve_live_cell(i, j);
+                } else { // else if dead
+                    future.evolve_dead_cell(i, j);
+                }
+            }
+        }
+    
+        // copy board back into self
+        self.update(&future);
     }
 
-    pub fn get_height(&self) -> usize {
-        self.height
+    // counts the number of neighbors around board element at (row,col) that are alive
+    fn count_live_neighbors(&mut self, row: usize, col: usize) -> usize {
+        // live nieghbor count
+        let mut n: usize = 0;
+    
+        // negatives are not allowed in ranges in rust :/
+        // so i have to shift things and make i32 copies of the usizes
+        let mut i: i32 = -1;
+        let mut j: i32 = -1;
+
+        let r: i32 = row.try_into().unwrap();
+        let c: i32 = col.try_into().unwrap();
+        let height: i32 = self.height.try_into().unwrap();
+        let width: i32 = self.width.try_into().unwrap();
+
+        // iterate through neighbors
+        while i < 2 {
+            while j < 2 {
+                if i == 0 && j == 0 { // if at reference element
+                    // do nothing (quick but lazy sorry)
+                } else {
+                    // if location is in board
+                    if r + i >= 0 && r + i < height && 
+                    c + j >= 0 && c + j < width {
+                        // if live
+                        // can only index with usize so must typecast
+                        if self.board[(r + i) as usize][(c + j) as usize] == true {
+                            n += 1; // increment live count
+                        }
+                    }
+                }
+
+                j += 1;
+            }
+
+            i += 1;
+            j = -1;
+        }
+        
+    
+        // return live count
+        n
     }
 
-    pub fn get_width(&self) -> usize {
-        self.width
+    fn evolve_live_cell(&mut self, row: usize, col: usize) {
+        let n: usize = self.count_live_neighbors(row, col);
+        if n < 2 || n > 3 { // if less than 2 or greater than 3 live neighbors
+            self.set_cell(false, row, col); // set to dead
+        }
+    }
+
+    fn evolve_dead_cell(&mut self, row: usize, col: usize) {
+        let n: usize = self.count_live_neighbors(row, col);
+        if n == 3 { // if exactly 3 live neighbors
+            self.set_cell(true, row, col); // set to live
+        }
     }
 }
 
 fn main() {
     let mut b_test = GameBoard::new(10,20);
-    b_test.print();
+    b_test.print(); // display empty board
     b_test.set_cell(true, 5, 5);
+    b_test.set_cell(true, 6, 5);
     b_test.set_cell(true, 6, 6);
     b_test.set_cell(true, 7, 7);
+    b_test.set_cell(true, 8, 8);
+    b_test.print(); // display starting board
+
+    // let it evolve a bit
+    b_test.evolve(); // 1
     b_test.print();
+    b_test.evolve(); // 2
+    b_test.print();
+    b_test.evolve(); // 3
+    b_test.print(); // (same as 2 because it is a stagnant square pattern)
 }
--- conways_game_of_life/target/debug/conways_game_of_life.d
+++ conways_game_of_life/target/debug/conways_game_of_life.d
@@ -1,1 +1,1 @@
-/home/vagrant/src/128h-final-project/rustseagull/conways_game_of_life/target/debug/conways_game_of_life: /home/vagrant/src/128h-final-project/rustseagull/conways_game_of_life/src/main.rs
+/home/vagrant/src/rustseagull/conways_game_of_life/target/debug/conways_game_of_life: /home/vagrant/src/rustseagull/conways_game_of_life/src/main.rs
