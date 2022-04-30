7e4825f3528de72f59759b4a580242a40d5cf748
03kkim <theq614@gmail.com>
2022-04-30 16:43:41 -0500

    fix clicking/dragging (still too thick)

--- conways_game_of_life/src/main.rs
+++ conways_game_of_life/src/main.rs
@@ -391,7 +391,7 @@ fn main() -> Result<(), pixels::Error> {
             if win_input.mouse_pressed(0) {
                 // debug!("Mouse click at {:?}", mouse_cell);
                 // set cell to alive
-                draw_state = Some(game_board.set_cell(true, mouse_cell.0.try_into().unwrap(), mouse_cell.1.try_into().unwrap()));
+                draw_state = Some(game_board.set_cell(true, mouse_cell.1.try_into().unwrap(), mouse_cell.0.try_into().unwrap()));
                 // game_board.evolve();
                 game_board.draw(pixels.get_frame());
                 pixels.render();
